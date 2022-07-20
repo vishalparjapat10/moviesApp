71816723655a920d5568e949ed776287a3f2c72f
vishalparjapat10 <vishalparjapat12527@gmail.com>
2022-07-21 00:28:49 +0530

    :construction: genres logic completed

--- src/Components/Favourites.js
+++ src/Components/Favourites.js
@@ -8,16 +8,58 @@ export default class Favourites extends Component {
         super();
         this.state = {
             movies:[],
+            genre:[],
+            curGenre:"All Genres"
         }
     }
 
+     handleGenre = async (genre) =>{
+        let ans = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+        let genreId={28:'Action',12:'Adventure',16:'Animation',35:'Comedy',80:'Crime',99:'Documentary',18:'Drama',10751:'Family',14:'Fantasy',36:'History',
+                        27:'Horror',10402:'Music',9648:'Mystery',10749:'Romance',878:'Sci-Fi',10770:'TV',53:'Thriller',10752:'War',37:'Western'}
+        let updatedMovies = ans.data.results;
+        
+        
+        
+        console.log(genre);
+        let genreMovies;
+        
+        if(genre !== 'All Genres'){
+            genreMovies = updatedMovies.filter((movie) =>{
+                if(genreId[movie.genre_ids[0]] == genre){
+                    console.log("Movie = ",movie);
+                    return movie;
+                }
+            });
+        }
+        else{
+            genreMovies = updatedMovies;
+        }
+        
+        console.log(genreMovies);
+        this.setState({
+            curGenre: genre,
+            movies: [...genreMovies]
+        })
+    }
     async componentDidMount(){
         console.log("component did mount is called");
         let ans = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
-        
+        let genreId={28:'Action',12:'Adventure',16:'Animation',35:'Comedy',80:'Crime',99:'Documentary',18:'Drama',10751:'Family',14:'Fantasy',36:'History',
+                        27:'Horror',10402:'Music',9648:'Mystery',10749:'Romance',878:'Sci-Fi',10770:'TV',53:'Thriller',10752:'War',37:'Western'}
+        let genreArr = [];
+        ans.data.results.map((movieObj) =>{
+            if(!genreArr.includes(genreId[movieObj.genre_ids[0]])){
+                genreArr.push(genreId[movieObj.genre_ids[0]])
+            }
+        });
+        genreArr.unshift("All Genres");
         this.setState({
             movies: [...ans.data.results], //[{},{},{}]
+            genre: [...genreArr]
         });
+
+        console.log('genre array = ',genreArr);
     }
   render() {
     let genreId={28:'Action',12:'Adventure',16:'Animation',35:'Comedy',80:'Crime',99:'Documentary',18:'Drama',10751:'Family',14:'Fantasy',36:'History',
@@ -26,10 +68,16 @@ export default class Favourites extends Component {
         <div class="row">
             <div class="col-3 favourites-list">
                 <ul class="list-group">
-                    <li class="list-group-item active" aria-current="true">All Genres</li>
-                    <li class="list-group-item">Fantasy</li>
-                    <li class="list-group-item">Action</li>
-                    <li class="list-group-item">Horror</li>
+                    {
+                        this.state.genre.map((genre)=>(
+                            this.state.curGenre == genre ?
+                            <li class="list-group-item active" aria-current="true">
+                                {genre}
+                            </li> :
+                            
+                            <li class="list-group-item" aria-current="true" onClick={() => this.handleGenre(genre)}>{genre}</li>
+                        ))
+                    }
                 </ul>
             </div>
             <div class="col favourites-table">
@@ -53,7 +101,7 @@ export default class Favourites extends Component {
                                 this.state.movies.map((movieObj) => (
                                     <tr>
                                         <td scope='row'>
-                                            <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} style={{ width: "8rem" }}/>
+                                            <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} style={{ width: "8rem" ,marginRight:"10px"}}/>
                                             {movieObj.original_title}
                                         </td>
                                         <td>{genreId[movieObj.genre_ids[0]]}</td>
